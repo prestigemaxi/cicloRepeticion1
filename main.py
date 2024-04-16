088df80824e1826a792b66b4a544b630a45433f6
maxi <CETECOM@AOAWSB02LC0826.duoc.acad>
2024-04-15 20:35:30 -0400

    subo archivo main

---
+++ main.py
@@ -0,0 +1,39 @@
+# import os
+# os .system("cls")
+        
+# #ciclo repeticion for
+
+# for x in range(3):
+    
+#     print(f"se portaron mal el sabado {x}")
+#     contadorNota = 0
+# try:
+#     cantidad = int(input("ingrese cantidad de notas a promediar\n"))
+#     for x in range(cantidad):
+#         nota = float(input(f"ingrese nota {x+1} \n"))
+#         contadorNota = contadorNota + nota
+#     promedio = contadorNota/cantidad 
+#     promedioRedondeado = round(promedio, 1)
+#     print(f"el resultado de{cantidad} notas es : {promedioRedondeado}")
+# except:
+#      print("tipo de datos no es compatible")
+     
+     
+bandera = True
+while bandera :
+    numero = int(input("ingrese un numero"))
+    if numero%2==0 :
+        print("puede elegir otro numero")
+ 
+    else:
+     print("el numeroes impar, se acabo el ciclo")  
+     bandera = False
+     print("muchas gracias por ocupar la app") 
+     a = 10
+     while a >= 10:
+         numero = int(input("ingrese numero"))
+     print("baicito")
+     
+    
+    
+    
